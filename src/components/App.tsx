e593c43cbfb4d028b6669858ef04e349995530c3
Oksana <oksanavasilevna77@gmail.com>
2025-06-27 19:26:16 +0300

    add assets

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,19 @@
-
+import { ToastContainer } from 'react-toastify';
+import { QuizProvider } from './QuizContext/QuizContext';
+import Header from './Header/Header';
+import SectionHero from './SectionHero/SectionHero';
 import './App.css';
 
 function App() {
   return (
     <>
-     
+      <Header />
+      <main>
+        <QuizProvider>
+          <SectionHero />
+        </QuizProvider>
+        <ToastContainer />
+      </main>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './components/App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import App from './components/App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
